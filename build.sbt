b1a7e465b80b25e0a8ee010051f3151280de299a
charlibot <charlibot@protonmail.com>
2020-04-13 00:49:17 +0200

    display list of pdfs

--- build.sbt
+++ build.sbt
@@ -19,7 +19,7 @@ libraryDependencies ++= Seq(
   "org.http4s"      %% "http4s-blaze-client" % Http4sVersion,
   "org.http4s"      %% "http4s-circe"        % Http4sVersion,
   "org.http4s"      %% "http4s-dsl"          % Http4sVersion,
-  "io.circe" %% "circe-generic" % CirceVersion,
+  "io.circe"        %% "circe-generic"       % CirceVersion,
   "dev.zio"         %% "zio-interop-cats"    % ZioCatsVersion,
   "com.github.pureconfig" %% "pureconfig"             % PureConfigVersion,
   "com.github.pureconfig" %% "pureconfig-cats-effect" % PureConfigVersion,
@@ -29,3 +29,11 @@ libraryDependencies ++= Seq(
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-Xcheckinit", "-encoding", "utf8", "-feature")
 
 addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
+
+enablePlugins(SbtWeb)
+
+// TODO: This doesn't work the way i want it.
+import ElmKeys._
+elmOutput in elmMake := (resourceDirectory in Compile).value / "js" / "elmMain.js"
+(compile in Compile) := ((compile in Compile) dependsOn elmMake).value
+
--- src/main/scala/com/charlibot/pdfviewer/Main.scala
+++ src/main/scala/com/charlibot/pdfviewer/Main.scala
@@ -2,7 +2,7 @@ package com.charlibot.pdfviewer
 
 import cats.effect.ExitCode
 import com.charlibot.pdfviewer.configuration.Configuration
-import com.charlibot.pdfviewer.http.Api
+import com.charlibot.pdfviewer.http.{Api, Views}
 import com.charlibot.pdfviewer.pdfs.Pdfs
 import com.charlibot.pdfviewer.ui.Ui
 import org.http4s.implicits._
@@ -10,13 +10,14 @@ import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.server.middleware.CORS
 import zio._
+import zio.blocking.Blocking
 import zio.clock.Clock
 import zio.console.putStrLn
 import zio.interop.catz._
 
 object Main extends App {
 
-  type AppEnvironment = Configuration with Ui with Clock with Pdfs
+  type AppEnvironment = Configuration with Ui with Clock with Blocking with Pdfs
 
   type AppTask[A] = RIO[AppEnvironment, A]
 
@@ -25,8 +26,10 @@ object Main extends App {
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
     val program: ZIO[AppEnvironment, Throwable, Unit] = for {
       api <- configuration.apiConfig
+      blockingEC <- blocking.blocking { ZIO.descriptor.map(_.executor.asEC) }
       httpApp = Router[AppTask](
-        "/" -> Api().route
+        "/api" -> Api().route,
+        "/" -> Views(blockingEC).route
       ).orNotFound
 
       server <- ZIO.runtime[AppEnvironment].flatMap { implicit rts =>
